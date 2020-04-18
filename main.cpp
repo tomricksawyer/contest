42a87efad0b2caffe47b70fe795f2b31561eb67b
hugecat0330 <weny55555@gmail.com>
2020-04-18 21:15:35 +0800

    add

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@
 #include <string>
 #include <vector>
 using namespace std;
-class linear{
-    public:
-        float x1;
-        float x2;
-        float y1;
-        float y2;
-        int type;//0 for verti , 1 for horiz ,2 for slash
-        linear(float x1,float y1 ,float x2,float y2) : x1(x1),y1(y1),x2(x2),y2(y2){};
-        
+class linear
+{
+public:
+    float x1;
+    float x2;
+    float y1;
+    float y2;
+    int type; //0 for verti , 1 for horiz ,2 for slash
+    linear(float x1, float y1, float x2, float y2, int type) : x1(x1), y1(y1), x2(x2), y2(y2), type(type){};
 };
-class Arc{
-    public:
-        float x1, x2, y1, y2, cx, cy;
-        bool direction; //true for CW; false for CCW
-        Arc(float x1,float y1,float x2,float y2,float cx,float cy,bool direction):x1(x1),y1(y1),x2(x2),y2(y2),cx(cx),cy(cy),direction(direction){};
+class Arc
+{
+public:
+    float x1, x2, y1, y2, cx, cy;
+    bool direction; //true for CW; false for CCW
+    Arc(float x1, float y1, float x2, float y2, float cx, float cy, bool direction) : x1(x1), y1(y1), x2(x2), y2(y2), cx(cx), cy(cy), direction(direction){};
 };
-int stringtoint(string str){
+int stringtoint(string str)
+{
     return str.compare("Line");
 }
 int main()
@@ -32,26 +34,33 @@ int main()
     float expand;
     float NotchSize;
     vector<linear> arr;
+    vector<Arc> arr2;
+    int type;
     while (true)
     {
         getline(cin, input);
         input.pop_back(); //get rid of ';'
         istringstream ss(input);
         string token;
-        float x1, y1, x2, y2;
+        float x1, y1, x2, y2, cx, cy;
+        bool rotation;
         getline(ss, token, ',');
-        if(token == "Data"){
+        if (token == "Data")
+        {
             getline(ss, data_index);
         }
-        else if(token=="Expand"){
-            getline(ss, _expand,',');
+        else if (token == "Expand")
+        {
+            getline(ss, _expand, ',');
             expand = stof(_expand);
         }
-        else if(token=="NotchSize"){
-            getline(ss, _NotchSize,',');
+        else if (token == "NotchSize")
+        {
+            getline(ss, _NotchSize, ',');
             NotchSize = stof(_NotchSize);
         }
-        else if(token=="Line"){
+        else if (token == "Line")
+        {
             string temp;
             getline(ss, temp, ',');
             x1 = stof(temp);
@@ -61,10 +70,17 @@ int main()
             y1 = stof(temp);
             getline(ss, temp, ',');
             y2 = stof(temp);
-            linear obj(x1, y1, x2, y2);
+            if (x1 == x2)
+                type = 1;
+            else if (y1 == y2)
+                type = 0;
+            else
+                type = 2;
+            linear obj(x1, y1, x2, y2, type);
             arr.push_back(obj);
         }
-        else if(token=="Arc"){
+        else if (token == "Arc")
+        {
             string temp;
             getline(ss, temp, ',');
             x1 = stof(temp);
@@ -74,7 +90,16 @@ int main()
             y1 = stof(temp);
             getline(ss, temp, ',');
             y2 = stof(temp);
-        }
+            getline(ss, temp, ',');
+            cx = stof(temp);
+            getline(ss, temp, ',');
+            cy = stof(temp);
+            getline(ss, temp, ',');
+            if (temp == "CW")
+                rotation = 1;
+            else if (temp == "CCW")
+                rotation = 0;
+            arr2.push_back(Arc(x1, x2, y1, y2, cx, cy, rotation));
         }
     }
 
